1618c6eca6775d7bea1220eccc293e84ddaf3568
shobha Mohan <121069703+shobhamohan@users.noreply.github.com>
2022-12-20 13:15:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 streamlit.title('My parents New Healthy Diner')
-streamlit.header('salads')
-streamlit.text('ceasar salad')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
