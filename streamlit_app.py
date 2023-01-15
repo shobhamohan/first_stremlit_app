c8ca84f3f3108e42f46e8b243481279c0d4d852e
shobha Mohan <121069703+shobhamohan@users.noreply.github.com>
2023-01-14 21:50:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,10 @@ try:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
+    
   except URLError as e:
     streamlit.error()
+    
 #streamlit.text(fruityvice_response.json())
 
 streamlit.title('My parents New Healthy Diner')
