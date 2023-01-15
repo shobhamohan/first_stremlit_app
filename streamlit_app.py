749377c59c2fca8af0f3c6903e863dbf73969624
shobha Mohan <121069703+shobhamohan@users.noreply.github.com>
2023-01-14 22:13:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ from urllib.error import URLError
 # new section to display api response
 streamlit.header('Fruity Vice Advice!')
 #create a funtion 
-def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+def get_fruityvice_data(fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 try:
