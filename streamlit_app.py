768f5f8111e418bb3c56c86fa53b591e94c3c409
shobha Mohan <121069703+shobhamohan@users.noreply.github.com>
2023-01-16 18:39:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,8 +77,8 @@ try:
     else:
         streamlit.button('add a fruit to the list')
         my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-        back_from_function_in = insert_row_snowflake(add_my_fruit)
-        streamlit.text(back_from_function_in)
+        back_from_function = insert_row_snowflake(add_my_fruit)
+        streamlit.text(back_from_function)
 except URLError as e:
     streamlit.error()
 
