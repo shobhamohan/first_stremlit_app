8eee3a53e5a41db57c3813a67f2ed2d042e2ea29
shobha Mohan <121069703+shobhamohan@users.noreply.github.com>
2023-01-16 21:14:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,23 @@ import requests
 import pandas
 from urllib.error import URLError
 
+streamlit.title('My parents New Healthy Diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥤Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list =my_fruit_list.set_index ('Fruit')
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+# Display the table on the page.
+# streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
+
 # new section to display api response
 streamlit.header('Fruity Vice Advice!')
 #create a funtion 
@@ -23,22 +40,7 @@ try:
 except URLError as e:
     streamlit.error()
 
-streamlit.title('My parents New Healthy Diner')
-streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥤Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞Avocado toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-#import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list =my_fruit_list.set_index ('Fruit')
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-# Display the table on the page.
-# streamlit.dataframe(my_fruit_list)
-streamlit.dataframe(fruits_to_show)
+
 
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
